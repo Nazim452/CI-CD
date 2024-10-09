a3a3ed3e0c7793a26e0fd2114af2906c23a750da
unknown <nazim45200@gmail.com>
2024-10-09 11:47:49 +0530

    Readme Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Nazim Learning GitHub Actions</h1>
+      <h2>Complete course with 2 projects</h2>
+    </>
+  )
+}
+
+export default App
